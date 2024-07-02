81028c6f525ac949430344b6deec081a71796871
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-07-03 00:35:12 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import QuizCard from "$lib/QuizCard.svelte";
+
+</script>
 <div class="p-4 grid gap-4 h-full grid-rows-[auto_1fr]">
   <nav class="grid">
     <input type="text" class="shadow rounded-md text-2xl p-3 border-4 border-transparent focus:shadow-2xl focus:border-gray-200 focus:rounded-xl !outline-none transition" placeholder="scrivi...">
@@ -6,10 +10,12 @@
   <main class="grid grid-cols-2 gap-4">
     <div class="border bg-white/50 p-4 rounded-md">
       <h1 class="text-green-400 font-bold text-3xl uppercase">vero</h1>
+      <QuizCard></QuizCard>
     </div>
     
     <div class="border bg-white/50 p-4 rounded-md">
       <h1 class="text-red-400 font-bold text-3xl uppercase">falso</h1>
+      <QuizCard></QuizCard>
     </div>
   </main>
 </div>
