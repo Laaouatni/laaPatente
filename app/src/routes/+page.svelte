a1f73cc1297f60c4311bb1ce120f79a349d19b4a
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-07-03 00:34:07 +0200

    simple design

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-4 grid gap-4 h-full grid-rows-[auto_1fr]">
+  <nav class="grid">
+    <input type="text" class="shadow rounded-md text-2xl p-3 border-4 border-transparent focus:shadow-2xl focus:border-gray-200 focus:rounded-xl !outline-none transition" placeholder="scrivi...">
+  </nav>
+  
+  <main class="grid grid-cols-2 gap-4">
+    <div class="border bg-white/50 p-4 rounded-md">
+      <h1 class="text-green-400 font-bold text-3xl uppercase">vero</h1>
+    </div>
+    
+    <div class="border bg-white/50 p-4 rounded-md">
+      <h1 class="text-red-400 font-bold text-3xl uppercase">falso</h1>
+    </div>
+  </main>
+</div>
