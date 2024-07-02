e473aed134dc051c0e63006e8552fe838710d5e3
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-07-03 01:38:12 +0200

    save

--- app/src/lib/getWordBoolPercentage.ts
+++ app/src/lib/getWordBoolPercentage.ts
@@ -1,8 +1,34 @@
-export function getWordBoolPercentage(quiz: any[], word: string) : { howManyFalse: number, howManyTrue: number, percentageTrue: number, percentageFalse: number } {
+export function getWordBoolPercentage(
+  quiz: any[],
+  word: string,
+): {
+  howManyFalse: number;
+  howManyTrue: number;
+  percentageTrue: number;
+  percentageFalse: number;
+  whoWins: boolean;
+  quizWithWordTrue: any[];
+  quizWithWordFalse: any[];
+} {
   const quizWithWord = quiz.filter((thisQuiz) => thisQuiz.q.includes(word));
-  const howManyTrue = quizWithWord.filter((thisQuiz) => thisQuiz.a === true).length;
-  const howManyFalse = quizWithWord.filter((thisQuiz) => thisQuiz.a === false).length;
+  const quizWithWordTrue = quizWithWord.filter(
+    (thisQuiz) => thisQuiz.a === true,
+  );
+  const quizWithWordFalse = quizWithWord.filter(
+    (thisQuiz) => thisQuiz.a === false,
+  );
+  const howManyTrue = quizWithWordTrue.length;
+  const howManyFalse = quizWithWordFalse.length;
   const percentageTrue = (howManyTrue / quizWithWord.length) * 100;
   const percentageFalse = (howManyFalse / quizWithWord.length) * 100;
-  return { howManyFalse, howManyTrue, percentageTrue, percentageFalse };
+  const whoWins = percentageTrue > percentageFalse;
+  return {
+    howManyFalse,
+    howManyTrue,
+    percentageTrue,
+    percentageFalse,
+    whoWins,
+    quizWithWordTrue,
+    quizWithWordFalse,
+  };
 }
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import QuizCard from "$lib/QuizCard.svelte";
   import quizData from "$lib/quizData";
   import { recursiveObjectToArray } from "$lib/recursiveObjectToArray";
   import { getWordBoolPercentage } from "$lib/getWordBoolPercentage";
@@ -22,13 +21,17 @@
   </nav>
 
   <main class="grid grid-cols-2 gap-4">
-    <div class="border bg-white/50 p-4 rounded-md">
+    <div class="border bg-white/50 p-4 rounded-md {wordBoolPercentageObject.whoWins ? "border-8 rounded-2xl shadow bg-green-400/10 border-green-500" : ""}">
       <h1 class="text-green-400 font-bold text-3xl uppercase">vero</h1>
 
-      <div class="grid gap-2 content-center justify-items-center h-full scale-150">
-        {#if wordBoolPercentageObject}
+      {#if wordBoolPercentageObject}
+        <div
+          class=" grid gap-2 content-center justify-items-center h-full scale-150"
+        >
           <div>
-            <span class="font-bold text-[6vw]">{wordBoolPercentageObject.howManyTrue}</span>
+            <span class="font-bold text-[6vw]"
+              >{wordBoolPercentageObject.howManyTrue}</span
+            >
             <span class="text-[2.5vw]">domande vere</span>
           </div>
           <div>
@@ -37,12 +40,31 @@
             </span>
             <span class="text-[3vw]"> % </span>
           </div>
-        {/if}
-      </div>
+        </div>
+      {/if}
     </div>
 
-    <div class="border bg-white/50 p-4 rounded-md">
+    <div class="border bg-white/50 p-4 rounded-md {!wordBoolPercentageObject.whoWins ? "border-8 rounded-2xl shadow bg-red-400/10 border-red-500" : ""}">
       <h1 class="text-red-400 font-bold text-3xl uppercase">falso</h1>
+
+      {#if wordBoolPercentageObject}
+        <div
+          class=" grid gap-2 content-center justify-items-center h-full scale-150"
+        >
+          <div>
+            <span class="font-bold text-[6vw]"
+              >{wordBoolPercentageObject.howManyFalse}</span
+            >
+            <span class="text-[2.5vw]">domande false</span>
+          </div>
+          <div>
+            <span class="font-bold text-[6vw]">
+              {wordBoolPercentageObject.percentageFalse.toFixed(2)}
+            </span>
+            <span class="text-[3vw]"> % </span>
+          </div>
+        </div>
+      {/if}
     </div>
   </main>
 </div>
